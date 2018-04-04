14221c7cd52d6df00b26795d4b7375eb0aec77dc
Jackson-mc <daliniues@gmail.com>
2018-04-04 19:09:55 -0400

    Minor changes to declaration structure

--- Part 3/assembly/declaration.java
+++ Part 3/assembly/declaration.java
@@ -2,12 +2,17 @@ package assembly;
 
 public class Declaration {
     public String varName;
-    public static int memLoc;
+    public static int memLoc = 0;
+    public int memoryLocation;
     public Declaration (String varName) {
         this.varName = varName;
+        this.memoryLocation = memLoc;
         //increment memlock
-        System.out.println( "added " + varName + " at location" + memLoc);
-        memLoc = memLoc+4;
+        System.out.println( "* added " + varName + " at location" + memLoc);
+        memLoc = memLoc+1;
         
+    }
+    public int getmemLoc() {
+        return memLoc;
     }
 }
