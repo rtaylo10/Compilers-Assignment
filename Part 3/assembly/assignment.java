b63bc2e167c414720b793ff15e0c37a2c34ce3e3
Jackson-mc <daliniues@gmail.com>
2018-04-04 22:02:07 -0400

    All assignments done

--- Part 3/assembly/assignment.java
+++ Part 3/assembly/assignment.java
@@ -48,24 +48,130 @@ public class Assignment {
         control = 1;
     }
     public Assignment(String assign, String a, int b, int op) {
-        this.variableOp1 = a;
-        this.b = b;
-        this.variableAss = assign;
-        this.op = op;
-        control = 2;
+        Assembler temp = new Assembler();
+        int instruction = temp.instructionTracker;
+        Declaration assignDec = temp.getDec(assign);
+        Declaration aDec = temp.getDec(a);
+        int fp = aDec.getmemLoc();
+        int memAssign = assignDec.memoryLocation;
+        int memA = aDec.memoryLocation;
+        int offsetAssign = fp-memAssign;
+        int offsetA = fp - memA;
+        //load address assign. load a into 1, b into 2, decide what math, load result into 3, St 3 to 0
+        System.out.println("* loading "+  assign);
+        System.out.println(instruction + ": LDA  0,-" + offsetAssign + "(5)");
+        instruction++;
+        System.out.println("* Loading " + a);
+        System.out.println(instruction + ": LD  1,-" + offsetA +"(5)");
+        instruction++;
+        System.out.println("* Loading " + b);
+        System.out.println(instruction + ": LDC  2," + b +"(2)");
+        instruction++;
+        //Decide what op to do
+        String opCode;
+        switch (op) {
+            case 0: opCode = "ADD";
+                break;
+            case 1: opCode = "SUB";
+                break;
+            case 2: opCode = "MUL";
+                break;
+            case 3: opCode = "DIV";
+                break;
+            default: System.out.println("unsupported operation. Defaulting to add");
+                opCode="ADD";
+                break;
+        }
+        System.out.println(instruction +":  " + opCode + "  3,1,2");
+        instruction++;
+        System.out.println(instruction +":  ST  3,0(0)");
+        instruction++;
+        temp.instructionTracker = instruction;
     }
     public Assignment(String assign, int a, String b, int op) {
-        this.a = a;
-        this.variableOp2 = b;
-        this.variableAss = assign;
-        this.op = op;
-        control = 3;
+        Assembler temp = new Assembler();
+        int instruction = temp.instructionTracker;
+        Declaration assignDec = temp.getDec(assign);
+        Declaration bDec = temp.getDec(b);
+        int fp = bDec.getmemLoc();
+        int memAssign = assignDec.memoryLocation;
+        int memB = bDec.memoryLocation;
+        int offsetAssign = fp-memAssign;
+        int offsetB = fp-memB;
+        //load address assign. load a into 1, b into 2, decide what math, load result into 3, St 3 to 0
+        System.out.println("* loading "+  assign);
+        System.out.println(instruction + ": LDA  0,-" + offsetAssign + "(5)");
+        instruction++;
+        System.out.println("* Loading " + a);
+        System.out.println(instruction + ": LDC  1," + a + "(1)" );
+        instruction++;
+        System.out.println("* Loading " + b);
+        System.out.println(instruction + ": LD  2,-" + offsetB +"(5)");
+        instruction++;
+        //Decide what op to do
+        String opCode;
+        switch (op) {
+            case 0: opCode = "ADD";
+                break;
+            case 1: opCode = "SUB";
+                break;
+            case 2: opCode = "MUL";
+                break;
+            case 3: opCode = "DIV";
+                break;
+            default: System.out.println("unsupported operation. Defaulting to add");
+                opCode="ADD";
+                break;
+        }
+        System.out.println(instruction +":  " + opCode + "  3,1,2");
+        instruction++;
+        System.out.println(instruction +":  ST  3,0(0)");
+        instruction++;
+        temp.instructionTracker = instruction;
     }
     public Assignment(String assign, String a, String b, int op) {
-        this.variableOp1 = a;
-        this.variableOp2 = b;
-        this.variableAss = assign;
-        this.op = op;
+        Assembler temp = new Assembler();
+        int instruction = temp.instructionTracker;
+        Declaration assignDec = temp.getDec(assign);
+        Declaration aDec = temp.getDec(a);
+        Declaration bDec = temp.getDec(b);
+        int fp = aDec.getmemLoc();
+        int memAssign = assignDec.memoryLocation;
+        int memA = aDec.memoryLocation;
+        int memB = bDec.memoryLocation;
+        int offsetAssign = fp-memAssign;
+        int offsetA = fp - memA;
+        int offsetB = fp-memB;
+        //load address assign. load a into 1, b into 2, decide what math, load result into 3, St 3 to 0
+        System.out.println("* loading "+  assign);
+        System.out.println(instruction + ": LDA  0,-" + offsetAssign + "(5)");
+        instruction++;
+        System.out.println("* Loading " + a);
+        System.out.println(instruction + ": LD  1,-" + offsetA +"(5)");
+        instruction++;
+        System.out.println("* Loading " + b);
+        System.out.println(instruction + ": LD  2,-" + offsetB +"(5)");
+        instruction++;
+        //Decide what op to do
+        String opCode;
+        switch (op) {
+            case 0: opCode = "ADD";
+                break;
+            case 1: opCode = "SUB";
+                break;
+            case 2: opCode = "MUL";
+                break;
+            case 3: opCode = "DIV";
+                break;
+            default: System.out.println("unsupported operation. Defaulting to add");
+                opCode="ADD";
+                break;
+        }
+        System.out.println(instruction +":  " + opCode + "  3,1,2");
+        instruction++;
+        System.out.println(instruction +":  ST  3,0(0)");
+        instruction++;
+        temp.instructionTracker = instruction;
         control = 4;
     }
     public Assignment(String assign, String a) {
@@ -77,7 +183,7 @@ public class Assignment {
         int memAssign = assignDec.memoryLocation;
         int memA = aDec.memoryLocation;
         int offsetAssign = fp-memAssign;
-        int offsetA = fp = memA;
+        int offsetA = fp - memA;
         System.out.println("* evalutating "+  assign + " = " + a);
         this.variableOp1 = a;
         this.variableAss = assign;
